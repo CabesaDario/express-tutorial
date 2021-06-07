4bad2e344277ba031124c57b6547426c05db2289
DaríoOjeda <darioojeda_1995@hotmail.com>
2021-06-07 20:31:00 +0200

    añadida la funcionalidad de editar y borrar tareas

---
+++ public/js/controller.js
@@ -0,0 +1,68 @@
+get_todos()
+
+function get_todos() {
+  var request = new XMLHttpRequest();
+  var requestURL = '/get_todos'
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    var todos = request.response;
+    printTodos(todos);
+  }
+}
+
+function printTodos(todos) {
+  var tablePending = document.getElementById("pending_table")
+  var tableComplete = document.getElementById("completed_table")
+
+  for (var i in todos) {
+    const task = todos[i]
+    const todo_id = task.id
+    const todo = task.text
+    let row = document.createElement("tr")
+    row.innerHTML = `<td>${todo} 
+                      <nav class='buttons'>
+                        <button class='edit-button'>Editar</button>
+                        <button class='delete-button'>Borrar</button>
+                      </nav>
+                    </td>`;
+    row.addEventListener("click", event => {
+      if(event.target.className == 'edit-button'){
+        editTask(todo_id)
+      }else if(event.target.className == 'delete-button'){
+        deleteTask(todo_id)
+      }else{
+        changeStateTask(task)
+      }
+    });   
+
+    if(todos[i].complete === 0){
+      tablePending.append(row)
+    }else{
+      tableComplete.append(row)
+    }
+  }
+}
+
+function editTask(todo_id) {
+  let nuevoTexto = prompt("¿Por qué desea editar la tarea?");
+  if(nuevoTexto){
+    var form = document.getElementById("edit_todo_form")
+    form.action = form.action + todo_id + `/${nuevoTexto}`;
+    form.submit()
+  }
+
+}
+
+function changeStateTask(todo) {
+  var form = document.getElementById("complete_todo_form")
+  form.action = form.action + todo.id + `/${todo.complete}`;
+  form.submit()
+}
+
+function deleteTask(todo_id){
+  var form = document.getElementById("delete_todo_form")
+  form.action = form.action + todo_id;
+  form.submit()
+}
--- routes.js
+++ routes.js
@@ -24,32 +24,78 @@ con.connect(function(err) {
     console.log("Connected!");
 }); 
 router.get('/', (req, res) => {
-    res.render("index", { task: task, complete: complete}); //estos datos podrian venir de una base de datos, el objeto TITULO QUIERO DECIR
+    res.render("index"); //estos datos podrian venir de una base de datos, el objeto TITULO QUIERO DECIR
 });
+router.get('/get_todos', (req, res) => {
+    const queryString = "SELECT * FROM tasks"
+    con.query(queryString, (err, rows, fields) => {
+      if (err) {
+        console.log("Failed to query at /get_todo: " + err)
+      }
+      console.log("Getting data from database at /get_todos")
+      res.json(rows)
+    })
+  })
+  
+router.post('/complete_todo/:id/:complete', (req, res) => {
+    const todo_id = req.params.id
+    const complete = req.params.complete
+    let newState = 1;
+    if(complete == 1){
+      newState = 0
+    }
+    const queryString = `UPDATE tasks SET complete = '${newState}' WHERE id = ?`
+    con.query(queryString, [todo_id], (err, rows, fields) => {
+      if (err) {
+        console.log("Failed to query at /complete_todo/: " + todo_id + " " +
+        err)
+      }
+      console.log("@/complete_todo/ Completing todo with id " + todo_id)
+      res.redirect('/')
+    })
+}) 
+router.post('/edit_todo/:id/:newtext', (req, res) => {
+  const todo_id = req.params.id
+  const newText = req.params.newtext
+  const queryString = `UPDATE tasks SET text = '${newText}' WHERE id = ?`
+  con.query(queryString, [todo_id], (err, rows, fields) => {
+    if (err) {
+      console.log("La query falló en /edit_todo/: " + todo_id + " " +
+      err)
+    }
+    console.log("@/edit_todo/ Se ha completado la edición del todo con id " + todo_id)
+    res.redirect('/')
+  })
+})   
+router.post('/delete_todo/:id', (req, res) => {
+  const todo_id = req.params.id
+  const queryString = `DELETE FROM tasks WHERE id = '${todo_id}'`
+  con.query(queryString, (err) => {
+    if (err) {
+      console.log("Fallo al intentar borrar la tarea con id " + todo_id + " " +
+      err)
+    }
+    console.log("@/complete_todo/ Tarea borrada con id " + todo_id)
+    res.redirect('/')
+  })
+}) 
+router.post('/add_todo', (req, res) => {
+    const todo = req.body.add_todo_input
+    const values = `("${todo}", 0)`;
+    const queryString = `INSERT INTO tasks (text, complete) VALUES ${values}`
+    con.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log("@/add_todo todo " + todo + " added.")
+      res.redirect('/')
+    })
+})
 
 router.get('/servicios', (req, res) => {
     res.render("servicios", {tituloServicios : "mi titulo dinámico de servicios"}) 
 });
-router.post('/addtask', function (req, res) {
-    var newTask = req.body.newtask;
-    if(newTask.length > 0){
-        task.push(newTask);
-        res.redirect("/");
-    }
-});
-router.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
 
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        task.splice(task.indexOf(completeTask), 1);
-    }else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {     
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
-    res.redirect("/");
-});
+
 
 module.exports = router
